e4edc6010c9196c066bd864d0a02ce9c618f1bc3
Sandra <sandra-leidel@gmx.de>
2021-01-14 19:16:53 +0100

    Funktionalität: Zeichnen und Hintergrundfarbe ändern

---
+++ public/assets/js/canvas.js
@@ -0,0 +1,68 @@
+/** Globale Variablen **/
+var x, y;                       // Position der Maus
+var r = 10;                     // Radius der gezeichneten Punkte
+var draw = false;
+var color = '#ffff00';          // aktuell ausgewählte Farbe
+var mode = 1;                   // 1 - paint; 2 - fill; 3 - shape
+
+/** Event-Handler **/
+document.addEventListener("mousemove", position_handler);
+document.addEventListener("mousedown", function (event) {
+    let canvas = document.getElementById("canvas");
+    if (event.x > canvas.offsetLeft && event.x < (canvas.offsetLeft + canvas.width) && event.y > canvas.offsetTop && event.y < (canvas.offsetTop + canvas.height)) {
+        switch (mode) {
+            case 1:
+                draw = true;
+                draw_circle();
+                break;
+            case 2:
+                fill();
+                break;
+            default:
+                break;
+        }
+    }
+});
+document.addEventListener("mouseup", function () {
+    draw = false;
+});
+
+
+/**
+ * korrigiert Maus-position auf screen zu Maus-Position auf canvas
+ * wenn Maus gedrückt gehalten wird, wird Zeichen-Funktion aufgerufen
+ * @param event
+ */
+function position_handler(event) {
+    let canvas = document.getElementById('canvas');
+    x = event.pageX - canvas.offsetLeft;
+    y = event.pageY - canvas.offsetTop;
+    if (draw) {
+        draw_circle();
+    }
+}
+
+
+/**
+ * Zeichnet Kreis auf Canvas
+ */
+
+function draw_circle() {
+    let canvas = document.getElementById('canvas');
+    let context = canvas.getContext("2d");
+    context.beginPath();
+    context.arc(x, y, r, 0, Math.PI * 2, false);
+    context.fillStyle = color;
+    context.fill();
+}
+
+
+/**
+ * Ändert Hintergrundfarbe
+ * TODO: Füllalgorithmus
+ */
+
+function fill() {
+    let canvas = document.getElementById('canvas');
+    canvas.style.backgroundColor = color;
+}
