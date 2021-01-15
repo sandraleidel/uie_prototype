463d16d26811aace6933e84cf959b16f89d6b5c5
Vincent Thiele <hey@thielegram.de>
2021-01-15 22:57:02 +0100

    EDIT case, language and style consistency

--- public/assets/js/canvas.js
+++ public/assets/js/canvas.js
@@ -1,64 +1,70 @@
 // Smooth Drawing by Nikhil Krishnan on Codepen (https://codepen.io/nikhil8krishnan/pen/NNyJGd)
 
 // Global Vars
-const c = document.getElementById("canvas");
-const ctx = c.getContext("2d");
+const c = document.getElementById('canvas');
+const ctx = c.getContext('2d');
 
 let currentPosition = { x: 0, y: 0 };
 
 // 1 - paint; 2 - fill; 3 - shape
 let activeMode = 1;
 let activeColor;
-let activeBrushSize; 
+let activeBrushSize;
 
-document.addEventListener("mousemove", draw);
-document.addEventListener("mousedown", updatePosition);
-document.addEventListener("mouseenter", updatePosition);
-
+document.addEventListener('mousemove', draw);
+document.addEventListener('mousedown', updatePosition);
+document.addEventListener('mouseenter', updatePosition);
 
 /**
- * Ändert Modus zwischen Zeichnen (1), Füllen (2) und Form-Modus (3)
- * @param new_mode
- * @param activeButton
+ * Set active mode (1) drawing, (2) filling, (3) shapes
+ *
+ * @param newMode selected mode
+ * @param activeButton pressed button, to be set active
  */
-function change_mode(new_mode, activeButton) {
-	activeMode = new_mode;
-	let mode_buttons = document.querySelectorAll(".btn-mode");
-	mode_buttons.forEach((button) => button.classList.remove("active"));
-	activeButton.classList.add("active");
-	if (activeMode == 3) {
-		alert("Formenfenster öffnen");
+function changeMode(newMode, activeButton) {
+	const modeButtons = document.querySelectorAll('.btn-mode');
+	modeButtons.forEach(button => button.classList.remove('active'));
+    
+    activeMode = newMode;
+	activeButton.classList.add('active');
+    
+    if (activeMode === 3) {
+		alert('Formenfenster öffnen');
 	}
 }
 
 /**
- * Ändert ausgewählte Farbe
- * @param new_color
- * @param activeButton
- */
-function change_color(new_color, activeButton) {
-	activeColor = new_color;
-	let color_buttons = document.querySelectorAll(".btn-color");
-	color_buttons.forEach((button) => button.classList.remove("active"));
-	activeButton.classList.add("active");
-}
-
-/**
- * ändert ausgewählte Größe
- * @param new_size - Radius
- * @param activeButton
- */
-function change_size(new_size, activeButton) {
-	activeBrushSize = new_size;
-	let size_buttons = document.querySelectorAll(".btn-size");
-	size_buttons.forEach((button) => button.classList.remove("active"));
-	activeButton.classList.add("active");
-}
-
-/**
- * Update mouse event position
+ * Set active color
  * 
- * @param {object} e  event
+ * @param newColor selected color
+ * @param activeButton pressed button, to be set active
+ */
+function changeColor(newColor, activeButton) {
+	const colorButtons = document.querySelectorAll('.btn-color');
+	colorButtons.forEach(button => button.classList.remove('active'));
+    
+    activeColor = newColor;
+	activeButton.classList.add('active');
+}
+
+/**
+ * Set active brush size
+ * 
+ * @param newBrushSize selected brush size 
+ * @param activeButton pressed button, to be set active
+ */
+function changeSize(newBrushSize, activeButton) {
+	const sizeButtons = document.querySelectorAll('.btn-size');
+	sizeButtons.forEach(button => button.classList.remove('active'));
+    
+    activeBrushSize = newBrushSize;
+	activeButton.classList.add('active');
+}
+
+/**
+ * Update mouse position
+ *
+ * @param {object} e event
  */
 function updatePosition(e) {
 	currentPosition.x = e.pageX - canvas.offsetLeft;
@@ -67,7 +73,7 @@ function updatePosition(e) {
 
 /**
  * Draw line in canvas
- * 
+ *
  * @param {object} e event
  */
 function draw(e) {
@@ -75,7 +81,7 @@ function draw(e) {
 
 	ctx.beginPath();
 	ctx.lineWidth = activeBrushSize;
-	ctx.lineCap = "round";
+	ctx.lineCap = 'round';
 	ctx.strokeStyle = activeColor;
 
 	ctx.moveTo(currentPosition.x, currentPosition.y);
@@ -86,10 +92,10 @@ function draw(e) {
 }
 
 /**
- * Ändert Hintergrundfarbe
- * TODO: Füllalgorithmus
+ * Change background color
+ * TODO: implement fill algorithm
  */
 function fill() {
-	let canvas = document.getElementById("canvas");
+	const canvas = document.getElementById('canvas');
 	canvas.style.backgroundColor = activeColor;
 }
