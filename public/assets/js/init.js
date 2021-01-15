758b4e29b465a149c2bf5c2a9f818a273ad064ff
Vincent Thiele <vincent_julius.thiele@mailbox.tu-dresden.de>
2021-01-15 13:10:52 +0100

    ADD color button generation

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -1,6 +1,17 @@
-const main = document.querySelector("main");
-const canvas = document.createElement("canvas");
-canvas.id = "canvas";
+const main = document.querySelector('main');
+const buttonWrapper = document.querySelector("#buttonWrapperColor");
+const canvas = document.createElement('canvas');
+
+colors.forEach(color => {
+    const button = document.createElement("button");
+    button.id = color.name;
+    button.classList.add("color");
+    button.onclick = () => { change_color(color.code, button) };
+
+    buttonWrapper.append(button);
+})
+
+canvas.id = 'canvas';
 canvas.width = main.clientWidth;
 canvas.height = main.clientHeight;
 
