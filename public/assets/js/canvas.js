f3550c38e35762f87868c19cfebf64abe3af0a65
Vincent Thiele <hey@thielegram.de>
2021-01-15 22:17:08 +0100

    EDIT structure

--- public/assets/js/canvas.js
+++ public/assets/js/canvas.js
@@ -1,55 +1,20 @@
 // Smooth Drawing by Nikhil Krishnan on Codepen (https://codepen.io/nikhil8krishnan/pen/NNyJGd)
 
-/** Globale Variablen **/
-var activeBrushSize; // Radius der gezeichneten Punkte
-var activeColor; // aktuell ausgewählte Farbe
-var mode = 1; // 1 - paint; 2 - fill; 3 - shape
-var c = document.getElementById("canvas");
-var ctx = c.getContext("2d");
+// Global Vars
+const c = document.getElementById("canvas");
+const ctx = c.getContext("2d");
 
-// Default position
-var DefaultPos = { x: 0, y: 0 };
+let currentPosition = { x: 0, y: 0 };
+
+// 1 - paint; 2 - fill; 3 - shape
+let activeMode = 1;
+let activeColor;
+let activeBrushSize; 
 
 document.addEventListener("mousemove", draw);
-document.addEventListener("mousedown", setPosition);
-document.addEventListener("mouseenter", setPosition);
+document.addEventListener("mousedown", updatePosition);
+document.addEventListener("mouseenter", updatePosition);
 
-//new mouse event position
-function setPosition(e) {
-	DefaultPos.x = e.pageX - canvas.offsetLeft;
-	DefaultPos.y = e.pageY - canvas.offsetTop;
-}
-
-//drawing canvas object
-function draw(e) {
-	//left click define
-	if (e.buttons !== 1) return;
-
-	ctx.beginPath(); //path begin
-	ctx.lineWidth = 5;
-	ctx.lineCap = "round";
-	ctx.strokeStyle = "white";
-
-	ctx.moveTo(DefaultPos.x, DefaultPos.y);
-	setPosition(e);
-	ctx.lineTo(DefaultPos.x, DefaultPos.y);
-
-	ctx.stroke();
-}
-
-/**
- * korrigiert Maus-position auf screen zu Maus-Position auf canvas
- * wenn Maus gedrückt gehalten wird, wird Zeichen-Funktion aufgerufen
- * @param event
- */
-function position_handler(event) {
-	let canvas = document.getElementById("canvas");
-	x = event.pageX - canvas.offsetLeft;
-	y = event.pageY - canvas.offsetTop;
-	if (draw) {
-		draw_circle();
-	}
-}
 
 /**
  * Ändert Modus zwischen Zeichnen (1), Füllen (2) und Form-Modus (3)
@@ -57,11 +22,11 @@ function position_handler(event) {
  * @param activeButton
  */
 function change_mode(new_mode, activeButton) {
-	mode = new_mode;
+	activeMode = new_mode;
 	let mode_buttons = document.querySelectorAll(".btn-mode");
 	mode_buttons.forEach((button) => button.classList.remove("active"));
 	activeButton.classList.add("active");
-	if (mode == 3) {
+	if (activeMode == 3) {
 		alert("Formenfenster öffnen");
 	}
 }
@@ -91,6 +56,16 @@ function change_size(new_size, activeButton) {
 }
 
 /**
+ * Update mouse event position
+ * 
+ * @param {object} e  event
+ */
+function updatePosition(e) {
+	currentPosition.x = e.pageX - canvas.offsetLeft;
+	currentPosition.y = e.pageY - canvas.offsetTop;
+}
+
+/**
  * Draw line in canvas
  * 
  * @param {object} e event
@@ -103,9 +78,9 @@ function draw(e) {
 	ctx.lineCap = "round";
 	ctx.strokeStyle = activeColor;
 
-	ctx.moveTo(DefaultPos.x, DefaultPos.y);
-	setPosition(e);
-	ctx.lineTo(DefaultPos.x, DefaultPos.y);
+	ctx.moveTo(currentPosition.x, currentPosition.y);
+	updatePosition(e);
+	ctx.lineTo(currentPosition.x, currentPosition.y);
 
 	ctx.stroke();
 }
