75e79a72cc1d87703fd251ff005e0517a20da9db
Vincent Thiele <vincent_julius.thiele@mailbox.tu-dresden.de>
2021-01-17 12:57:46 +0100

    ADD dummy menu

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -1,4 +1,4 @@
-const main = document.querySelector('main');
+const main = document.querySelector('#canvasContainer');
 const buttonWrapperColor = document.querySelector('#buttonWrapperColor');
 const buttonWrapperMode = document.querySelector('#buttonWrapperMode');
 const canvas = document.createElement('canvas');
